92c1376ad0093d57d4fe1cc47afd68c6020e043e
lastreliance <90848871+lastreliance@users.noreply.github.com>
2022-12-02 08:53:37 +0800

    added init_ui method

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import tkinter as tk
+import settings
+
+from container import Container
 
 
 class App:
     # noinspection PyTypeChecker
     def __init__(self):
         self.window: tk.Tk = None
+        self.containers: List[Container] = list()
+    
+    def init_ui(self):
+        self.window = tk.Tk()
+        self.window.geometry = settings.geometry
+        self.containers = [Container()]
 
     def start(self):
         self.init_ui()
