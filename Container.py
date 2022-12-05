2daf8605cfbefe9caeddfc42e0bcd770ceb6783a
lastreliance <chinger1231@gmail.com>
2022-12-06 03:38:32 +0800

    2 commit

--- Container.py
+++ Container.py
@@ -5,6 +5,7 @@ from typing import List
 class Container:
     def __init__(self):
         self.widgets: List[tk.Widget] = list()
+        self.places: List[tuple] = list()
 
     def destroy(self):
         for widget in self.widgets[::-1]:
@@ -13,13 +14,26 @@ class Container:
 
     def add(self, widget: tk.Widget):
         self.widgets.append(widget)
+        info = widget.place_info()
+        self.places.append((int(info['x']), int(info['y'])))
 
     def hide(self):
-        for widget in self.widgets:
-            widget.place_info()
+        for i in range(len(self.widgets)):
+            self.places[i] = self.widgets[i].place_info()
+            self.widgets[i].place_forget()
+
+    def show(self):
 
 
-class WindowContainer(Container):
+
+class WindowContainer:
     def __init__(self):
-        self.windows: List[tk.TopLevel] = list()
-        
+        self.widgets: List[tk.Toplevel] = list()
+
+    def add(self, window: tk.Toplevel):
+        self.widgets.append(window)
+
+    def destroy(self):
+        for widget in self.widgets[::-1]:
+            widget.destroy()
+            self.widgets.pop()
